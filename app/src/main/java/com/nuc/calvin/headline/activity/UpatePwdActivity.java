2e96927143eb6f0aa939c46d16c861a0572f80b6
chen <m15903462872@163.com>
2019-07-01 15:09:41 +0800

    update code

---
+++ app/src/main/java/com/nuc/calvin/headline/activity/UpatePwdActivity.java
@@ -0,0 +1,15 @@
+package com.nuc.calvin.headline.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.nuc.calvin.headline.R;
+
+public class UpatePwdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upate_pwd);
+    }
+}
